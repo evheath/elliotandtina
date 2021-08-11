7fb296bc2b32a564d4cd51fa234c3ccf40118410
evheath <elliot.v.heath@gmail.com>
2021-08-11 09:35:13 -0500

    reactive rsvp form

--- src/app/rsvp-page/rsvp-page.component.ts
+++ src/app/rsvp-page/rsvp-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { SnackbarService } from '../snackbar.service';
 import { RsvpDataModel } from './rsvp.model';
 
@@ -11,14 +12,15 @@ import { RsvpDataModel } from './rsvp.model';
   styleUrls: ['./rsvp-page.component.scss']
 })
 export class RsvpPageComponent implements OnInit {
-  public rsvpData: RsvpDataModel;
+  // public rsvpData: RsvpDataModel;
+  public rsvpForm: FormGroup;
 
-  constructor(private db: AngularFirestore, private auth: AngularFireAuth, private snack: SnackbarService) { }
+  constructor(private db: AngularFirestore, private auth: AngularFireAuth, private snack: SnackbarService, private fb: FormBuilder) { }
 
-  @ViewChild('inputName') inputName;
-  @ViewChild('inputAttending') inputAttending;
-  @ViewChild('inputAttendees') inputAttendees;
-  @ViewChild('inputComment') inputComment;
+  // @ViewChild('inputName') inputName;
+  // @ViewChild('inputAttending') inputAttending;
+  // @ViewChild('inputAttendees') inputAttendees;
+  // @ViewChild('inputComment') inputComment;
 
   // need for canDeactivate
   @HostListener('window:beforeunload', ['$event'])
@@ -29,46 +31,58 @@ export class RsvpPageComponent implements OnInit {
   }
 
   public hasUnsavedData(): boolean {
-    return this.inputName.dirty
-      || this.inputAttending.dirty
-      || (this.rsvpData.attending && this.inputAttendees.dirty)
-      || this.inputComment.dirty
+    return this.rsvpForm.dirty
+    // return this.inputName.dirty
+    //   || this.inputAttending.dirty
+    //   || (this.rsvpData.attending && this.inputAttendees.dirty)
+    //   || this.inputComment.dirty
   }
 
-  private markFormPristine() {
-    this.inputName.control.markAsPristine()
-    this.inputAttending.control.markAsPristine()
-    if (this.rsvpData.attending) {
-      // if statement needed since attendees is rendered conditionally
-      this.inputAttendees.control.markAsPristine()
-    }
-    this.inputComment.control.markAsPristine()
-  }
+  // private markFormPristine() {
+  //   this.inputName.control.markAsPristine()
+  //   this.inputAttending.control.markAsPristine()
+  //   if (this.rsvpData.attending) {
+  //     // if statement needed since attendees is rendered conditionally
+  //     this.inputAttendees.control.markAsPristine()
+  //   }
+  //   this.inputComment.control.markAsPristine()
+  // }
 
   public async writeRsvp() {
-    if (this.rsvpData.attendees < 0) {
-      this.snack.simple("No negative numbers haha");
-      this.rsvpData.attendees = 1
-      return;
-    }
-    if (this.rsvpData.attendees > 12) {
-      this.snack.simple("Keep it under 12 please");
-      this.rsvpData.attendees = 1
-      return;
-    }
-
+    // if (this.rsvpData.attendees < 0) {
+    //   this.snack.simple("No negative numbers haha");
+    //   this.rsvpData.attendees = 1
+    //   return;
+    // }
+    // if (this.rsvpData.attendees > 12) {
+    //   this.snack.simple("Keep it under 12 please");
+    //   this.rsvpData.attendees = 1
+    //   return;
+    // }
     const user = await this.auth.currentUser;
     const rsvpRef = this.db.doc<RsvpDataModel>(`rsvp/${user.uid}`)
-    this.rsvpData.uid = user.uid
-    this.rsvpData.email = user.email
-    this.rsvpData.phoneNumber = user.phoneNumber
-    await rsvpRef.set(this.rsvpData, { merge: true }).then(success => {
+
+    rsvpRef.update({
+      ...this.rsvpForm.value,
+      'uid': user.uid,
+    }).then(success => {
       this.snack.simple("RSVP updated");
-      this.markFormPristine();
+      this.rsvpForm.markAsPristine()
     }).catch(e => {
       this.snack.simple("Problem updating RSVP");
       console.error(e)
     })
+
+    // this.rsvpData.uid = user.uid
+    // this.rsvpData.email = user.email
+    // this.rsvpData.phoneNumber = user.phoneNumber
+    // await rsvpRef.set(this.rsvpData, { merge: true }).then(success => {
+    //   this.snack.simple("RSVP updated");
+    //   this.markFormPristine();
+    // }).catch(e => {
+    //   this.snack.simple("Problem updating RSVP");
+    //   console.error(e)
+    // })
 
   }
 
@@ -76,19 +90,32 @@ export class RsvpPageComponent implements OnInit {
     this.auth.currentUser.then(user => {
       const rsvpDoc = this.db.doc<RsvpDataModel>(`rsvp/${user.uid}`)
       rsvpDoc.get().toPromise().then(snap => {
-        this.rsvpData = snap.data()
+        // this.rsvpData = snap.data()
+        this.rsvpForm.patchValue(snap.data())
       })
     })
   }
 
-  ngOnInit() {
-    this.rsvpData = {
+  private buildForm() {
+    this.rsvpForm = this.fb.group({
       uid: "",
       name: "Lance Shmucketelly",
       attendees: 1,
       attending: false,
       comment: "",
-    }
+    })
+  }
+
+  ngOnInit() {
+    // this.rsvpData = {
+    //   uid: "",
+    //   name: "Lance Shmucketelly",
+    //   attendees: 1,
+    //   attending: false,
+    //   comment: "",
+    // }
+
+    this.buildForm();
 
     this.getExistingRsvp()
   }
