c8ed115d8a642bea595a42510a1f02c6c2fe2b89
evheath <elliot.v.heath@gmail.com>
2021-08-13 11:21:39 -0500

    rsvp onUpdate function

--- functions/src/firestore.ts
+++ functions/src/firestore.ts
@@ -36,5 +36,66 @@ export const onRsvpDocumentCreation = functions.firestore.document(`rsvp/{rsvpId
     next.namesOut = [name, ...namesOut];
   }
 
-  return aggRef.set(next);
+  return aggRef.set(next, { merge: true });
+});
+
+export const onRsvpDocumentUpdate = functions.firestore.document(`rsvp/{rsvpId}`).onUpdate(async (snapshot, context) => {
+  const incomingData = snapshot.after.data();
+  const existingData = snapshot.before.data();
+
+  // no meaningful change
+  if (existingData.attending === incomingData.attending && existingData.attendees === incomingData.attendees) {
+    return null;
+  }
+
+  // there was a meaningful change, so we can start getting/setting data
+  const aggRef = db.doc('aggregations/rsvp');
+  const aggDoc = await aggRef.get();
+  const aggData = aggDoc.data();
+  let attendeeCount = 0;
+  let namesIn: string[] = [];
+  let namesOut: string[] = [];
+  if (aggData) {
+    attendeeCount = aggData.attendeeCount;
+    namesIn = aggData.namesIn;
+    namesOut = aggData.namesOut;
+  }
+  const next = {
+    attendeeCount,
+    namesIn,
+    namesOut
+  };
+
+  // change from yes to no
+  if (existingData.attending === true && incomingData.attending === false) {
+    // remove name from namesIn
+    const index = next.namesIn.indexOf(incomingData.name);
+    if (index > -1) {
+      next.namesIn.splice(index, 1);
+    }
+    // add name to namesOut
+    next.namesOut = [incomingData.name, ...next.namesOut];
+    // subtract attendeeCount
+    next.attendeeCount -= existingData.attendees;
+  }
+  // change from no to yes
+  if (existingData.attending === false && incomingData.attending === true) {
+    // remove name from namesOut
+    const index = next.namesOut.indexOf(incomingData.name);
+    if (index > -1) {
+      next.namesOut.splice(index, 1);
+    }
+    // add name to namesIn
+    next.namesIn = [incomingData.name, ...next.namesIn];
+    // add to attendeeCount
+    next.attendeeCount += incomingData.attendees;
+  }
+  // already marked as attending, but a change in number of attendees
+  if (existingData.attending && incomingData.attending && existingData.attendees !== incomingData.attendees) {
+    next.attendeeCount -= existingData.attendees;
+    next.attendeeCount += incomingData.attendees;
+  }
+
+
+  return aggRef.set(next, { merge: true });
 });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
 export {
-  // onRsvpDocumentUpdate,
+  onRsvpDocumentUpdate,
   onRsvpDocumentCreation,
 } from './firestore';
